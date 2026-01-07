6eda70c0b6ddaed33fdf30db845550303e39016b
Pavel2011 <56668598+Pavel2011@users.noreply.github.com>
2026-01-07 15:37:53 +0300

    Add files via upload
    => Kantilena

--- placemark.js
+++ placemark.js
@@ -1,21 +1,22 @@
 ymaps.ready(init)
 function init() {
   let myMap = new ymaps.Map('ah-map', {
-    center: [55.095874, 36.517316],
+    center: [55.807845, 37.813558],
     zoom: 16,
   })
 
   let placemark = new ymaps.Placemark(
-    [55.095874, 36.517316],
+    [55.807845, 37.813558],
     {
       // baloonContentHeader:'ООО Агрохолдинг',
       // balloonContentBody:'ООО \"Агрохолдинг\"'
       balloonContent: `
   <div class='balloon'>
-  <div class='ball-name'>СНТ \"Венера\"</div>
-  <div class='ball-phone'>Телефон\:+ 7 ...</div>
-  <div class='ball-phone'>Телефон\:+ 7 ...</div>
-  <div class='ball-address'>Адрес\:...</div>
+  <div class='ball-name'>Вокально-хоровая студия «Кантилена»</div>
+  <div class='ball-phone'>Телефон\:, 
+         </div>
+  <div class='ball-phone'>Телефон\:</div>
+  <div class='ball-address'>Адрес\:Москва, 13-я Парковая улица, 38к3</div>
   </div>
   `,
     },
